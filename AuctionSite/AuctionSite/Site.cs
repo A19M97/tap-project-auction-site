c84584b719ddfae12c96f47af7e7f29a94ba9f9d
Andrea Mugnai <andrea.mugnai97@gmail.com>
2020-03-24 17:53:51 +0100

    Testable Classes with not implemented methods added.

---
+++ AuctionSite/AuctionSite/Site.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using TAP2018_19.AuctionSite.Interfaces;
+
+namespace AuctionSite
+{
+    public class Site : ISite
+    {
+        public IEnumerable<IUser> GetUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ISession> GetSessions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IAuction> GetAuctions(bool onlyNotEnded)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISession Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISession GetSession(string sessionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateUser(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CleanupSessions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Name { get; }
+        public int Timezone { get; }
+        public int SessionExpirationInSeconds { get; }
+        public double MinimumBidIncrement { get; }
+    }
+}
