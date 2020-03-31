693a3c8e1597e7ecfbbfb5fa854e43713b0c1cb7
Andrea Mugnai <andrea.mugnai97@gmail.com>
2020-03-31 21:33:10 +0200

    Auxiliary methods added.

--- AuctionSite/AuctionSite/_aux/utils/utils.cs
+++ AuctionSite/AuctionSite/_aux/utils/utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TAP2018_19.AuctionSite.Interfaces;
 
 namespace Mugnai._aux.utils
 {
@@ -11,6 +12,30 @@ namespace Mugnai._aux.utils
         public static bool SiteNameAlreadyExists(AuctionSiteContext context, string name)
         {
             return context.Sites.Any(site => site.Name == name);
+        }
+
+        public static bool IsEndedAuction(IAuction auction)
+        {
+            return auction.EndsOn < DateTime.Now;
+        }
+
+        public static bool IsSiteDisposed(Site site)
+        {
+            return site.IsDeleted;
+        }
+
+        internal static bool ArePasswordsEquals(string password1, string password2)
+        {
+            return password1 == password2;
+        }
+
+        internal static Session CreateNewSession(Site site, User user)
+        {
+            return new Session
+            {
+                ValidUntil = site.AlarmClock.Now.AddSeconds(site.SessionExpirationInSeconds),
+                User = user
+            };
         }
     }
 }
