112cdc16a6b794e277018845eb534e90347e8139
Andrea Mugnai <andrea.mugnai97@gmail.com>
2020-04-10 11:38:13 +0200

    Implementation for CreateSiteOnDb added.

--- AuctionSite/AuctionSiteLogic/SiteFactory.cs
+++ AuctionSite/AuctionSiteLogic/SiteFactory.cs
@@ -36,7 +36,35 @@ namespace Mugnai
         public void CreateSiteOnDb(string connectionString, string name, int timezone, int sessionExpirationTimeInSeconds,
             double minimumBidIncrement)
         {
-            throw new NotImplementedException();
+            if (null == connectionString || null == name)
+                throw new ArgumentNullException();
+
+            if (!IsValidSiteName(name))
+                throw new ArgumentException();
+
+            if (!IsValidTimezone(timezone))
+                throw new ArgumentOutOfRangeException();
+
+            if (!IsPositiveSessionExpiration(sessionExpirationTimeInSeconds) || !IsPositiveMinimumBidIncrement(minimumBidIncrement))
+                throw new ArgumentOutOfRangeException();
+
+            using (var context = new AuctionSiteContext(connectionString))
+            {
+                if (!ExistsDb(context))
+                    throw new UnavailableDbException();
+                if (Utils.SiteNameAlreadyExists(context, name))
+                    throw new NameAlreadyInUseException(name);
+
+                var site = new Site()
+                {
+                    Name = name,
+                    Timezone = timezone,
+                    SessionExpirationInSeconds = sessionExpirationTimeInSeconds,
+                    MinimumBidIncrement = minimumBidIncrement
+                };
+                context.Sites.Add(site);
+                context.SaveChanges();
+            }
         }
 
         public ISite LoadSite(string connectionString, string name, IAlarmClock alarmClock)
@@ -48,5 +76,33 @@ namespace Mugnai
         {
             throw new NotImplementedException();
         }
+
+        /* AUX METHODS*/
+        private static bool ExistsDb(AuctionSiteContext context)
+        {
+            return context.Database.Exists();
+        }
+
+        private static bool IsPositiveMinimumBidIncrement(double minimumBidIncrement)
+        {
+            return minimumBidIncrement > 0;
+        }
+
+        private static bool IsPositiveSessionExpiration(int sessionExpirationTimeInSeconds)
+        {
+            return sessionExpirationTimeInSeconds > 0;
+        }
+
+        private static bool IsValidTimezone(int timezone)
+        {
+            return timezone >= DomainConstraints.MinTimeZone && timezone <= DomainConstraints.MaxTimeZone;
+        }
+
+        private static bool IsValidSiteName(string name)
+        {
+            var nameLength = name.Length;
+            return nameLength >= DomainConstraints.MinSiteName && nameLength <= DomainConstraints.MaxSiteName;
+        }
+        /* END AUX METHODS*/
     }
 }
